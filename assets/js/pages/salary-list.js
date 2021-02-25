f99b87163755bda4cf001d6ce512ad191a3133e0
Gandhi Selvaraj <gandhidhamayanthi@gmail.com>
2021-02-25 22:47:52 +0530

    Date updated

--- assets/js/pages/salary-list.js
+++ assets/js/pages/salary-list.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     var y = d.getFullYear();
     var ym = y + "-" + m;
     $("#month-year").val(ym);
+    $("#month-year").attr('max', ym);
     displaySalaryListInit(m, y);
 });
 
